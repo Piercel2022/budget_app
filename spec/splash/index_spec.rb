916ace5e7c8887068e9980d08d67482870baca66
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-05-01 18:06:21 +0200

    Add required changes

---
+++ spec/splash/index_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe 'Splashes', type: :request do
+    describe 'GET /splash' do
+      it 'returns http success' do
+        get root_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
