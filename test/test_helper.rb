9ac2d124034863a9d7e1e3ae27f4e972460c1c6e
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-05-06 10:59:24 +0200

    Add the required changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,18 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-<<<<<<< HEAD
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-
-    # Add more helper methods to be used by all tests here...
-  end
-=======
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
@@ -22,5 +10,4 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
->>>>>>> a6bec44 (Add the workflows)
 end
