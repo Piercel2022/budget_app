f8a8b1df4bffc8783f3e1b0d6785fec898a9e21b
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-05-06 10:57:16 +0200

    Add the required changes

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,17 +1,5 @@
 require 'test_helper'
 
-<<<<<<< HEAD
-module ApplicationCable
-  class ConnectionTest < ActionCable::Connection::TestCase
-    # test "connects with cookies" do
-    #   cookies.signed[:user_id] = 42
-    #
-    #   connect
-    #
-    #   assert_equal connection.user_id, "42"
-    # end
-  end
-=======
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
   # test "connects with cookies" do
   #   cookies.signed[:user_id] = 42
@@ -20,5 +8,4 @@ class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
   #
   #   assert_equal connection.user_id, "42"
   # end
->>>>>>> a6bec44 (Add the workflows)
 end
