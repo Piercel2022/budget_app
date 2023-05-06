4e351001faeeecc20c83741d067f1b7473ca5bc0
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-05-06 10:33:35 +0200

    Add the required changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,9 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+  #Disable serving static files from the `/public` folder by default since
+  #Apache or NGINX already handles this.
+    config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
